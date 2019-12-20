67277bbb128dee79a252766c3904dfd92683753f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-12-20 13:24:32 +0100

    finito punto 11

--- Esercizio_Vacanze/Program.cs
+++ Esercizio_Vacanze/Program.cs
@@ -13,20 +13,22 @@ namespace Esercizio_Vacanze
             {
                 Console.WriteLine(a + 1);//l'output per essere giusto deve essere uguale a 2147483647, quindi in tutti i casi in cui il risultato è diverso da 2147483647, l'istruzione della riga 14 non viene effettuata
             }
-                int b = -128;
-                int c = -b;
+            Console.WriteLine("inserisci un numero");
+            int b = int.Parse(Console.ReadLine());
+            int c = -b;
             Console.WriteLine(Convert.ToString(b, 2));
-                Console.WriteLine(Convert.ToString(c, 2));
-                int value = 31;
-                int somma = value;
-                value = ~value + 1;//NOT
-                                   //value=value+1;
-                somma = somma + value;
-                Console.WriteLine($"{value} e {somma}");
-                long number = 55555;
-                Console.WriteLine(Convert.ToString(number, 2));
-                short number1 = (short)number;
-                Console.WriteLine(number1);
+            Console.WriteLine(Convert.ToString(c, 2));
+            Console.WriteLine(" nelle 2 righe sopra viene prima visulizzato il numero scelto da te in base binaria, mentre nella riga sotto viene visualizzato l'opposto del numero citato prima");
+            int value = 31;
+            int somma = value;
+            value = ~value + 1;//NOT
+                               //value=value+1;
+            somma = somma + value;
+            Console.WriteLine($"{value} e {somma}");
+            long number = 55555;
+            Console.WriteLine(Convert.ToString(number, 2));
+            short number1 = (short)number;
+            Console.WriteLine(number1);
             
         }
     }
